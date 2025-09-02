fb5c8f753505ca0e48c4683c0efb83e2f8853dbb
Sergio <71974638+salvar3nga@users.noreply.github.com>
2025-09-02 17:12:04 +0200

    feat: define prod base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
+const isProd = process.env.NODE_ENV === "production";
 export default defineConfig({
+  base: isProd ? "/salvar3nga.github.io/Quizz/" : "/",
   plugins: [svelte()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-})
+});
