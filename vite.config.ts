f3172ea800b95a5ce0ba76643c2eb8a00ea5bd01
Sergio <71974638+salvar3nga@users.noreply.github.com>
2025-09-02 17:58:58 +0200

    feat: edit deployment pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
-const isProd = process.env.NODE_ENV === "production";
 export default defineConfig({
-  base: isProd ? "/Quizz/" : "/",
+  base: "/Quizz/",
   plugins: [svelte()],
   resolve: {
     alias: {
