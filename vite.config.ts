fc8a9fc9c959887c79f77357238193b22674d560
Sergio <71974638+salvar3nga@users.noreply.github.com>
2025-09-02 17:18:03 +0200

    feat: experiment with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vite.dev/config/
 const isProd = process.env.NODE_ENV === "production";
 export default defineConfig({
-  base: isProd ? "/salvar3nga.github.io/Quizz/" : "/",
+  base: isProd ? "/Quizz/" : "/",
   plugins: [svelte()],
   resolve: {
     alias: {
